46159b954531ce374dd08fefccd7245718eccb54
Christoph Hanf <hanf@parimeo.com>
2014-01-25 21:30:34 +0100

    Starting project and implementing first User Story

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,16 @@
 'use strict';
 
-angular.module('parimeoFreelancerAppApp', [
-  'ngCookies',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('parimeoFreelancerAppApp', ['ngCookies', 'ngSanitize', 'ngRoute'])
+    .config(
+        ['$routeProvider',
+            function ($routeProvider) {
+                $routeProvider
+                    .when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
+                    .when('/home', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
+                    .when('/contact', {templateUrl: 'views/contact.html', controller: 'MainCtrl'})
+                    .when('/imprint', {templateUrl: 'views/imprint.html', controller: 'MainCtrl'})
+                    .when('/projects', {templateUrl: 'views/projects.html', controller: 'MainCtrl'})
+                    .when('/consultants', {templateUrl: 'views/consultants.html', controller: 'MainCtrl'})
+                    .when('/myprofile', {templateUrl: 'views/myprofile.html', controller: 'MainCtrl'})
+                    .otherwise({redirectTo: '/'});
+            }]);
