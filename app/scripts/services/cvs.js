2247ec016e4f1d6221f7e5526d07391d334d4ae8
Christoph Hanf <hanf@parimeo.com>
2014-03-14 13:18:04 +0100

    Finishing REST Service for CVs (GET consultant/:id/cvs/:id)

--- app/scripts/services/cvs.js
+++ app/scripts/services/cvs.js
@@ -24,10 +24,12 @@ app.factory('ConsultantCVService',
                 'http://10.211.55.6/restsvc01/consultants/:ConsultantId/cvs/:CvId',
                 {ConsultantId: '@ConsultantId', CvId: '@CvId'},
                 {'get': {method: 'GET', isArray: true, headers: {'Accept': 'application/json'}},
-                    'query': {method: 'GET', isArray: true, headers: {'Accept': 'application/json'}},
-                    'save': {method: 'PUT'},
-                    'add': {method: 'POST'},
-                    'insert': {method: 'POST'},
-                    'remove': {method: 'DELETE'},
-                    'delete': {method: 'DELETE'}});
+                    'query': {method: 'GET', isArray: true, headers: {'Accept': 'application/json'}}
+// currently not supported
+//                    'save': {method: 'PUT'},
+//                    'add': {method: 'POST'},
+//                    'insert': {method: 'POST'},
+//                    'remove': {method: 'DELETE'},
+//                    'delete': {method: 'DELETE'}
+                });
         }]);
